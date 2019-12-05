3046c3326fd62aa6e0ac8c99f12acb09dd4dfe10
akmal <akmal_hakim_teo@hotmail.com>
2019-12-05 18:35:20 +0800

    //added facial recognition CAMERA ONLY

--- app/src/main/java/com/example/myapplication/ManagerActivity.java
+++ app/src/main/java/com/example/myapplication/ManagerActivity.java
@@ -2,11 +2,13 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -16,13 +18,25 @@ public class ManagerActivity extends AppCompatActivity {
 
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         final Activity activity = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
         Button qrcodeButton = findViewById(R.id.qrCodeButton);
         Button button = findViewById(R.id.createEvent);
+        Button toFRpage = findViewById(R.id.facialrecognitionButton);
+        toFRpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ManagerActivity.this, FR_CAMERA.class);
+                startActivity(intent);
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(ManagerActivity.this, AddEventActivity.class);
