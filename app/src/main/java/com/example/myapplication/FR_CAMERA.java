8ecd264bc36e1bd55409a399a4da2b57eee4ebd6
akmal <akmal_hakim_teo@hotmail.com>
2019-12-05 19:42:41 +0800

    //added facial recognition CAMERA ONLY to register page and facial recognition page //TODO pics to serverside

--- app/src/main/java/com/example/myapplication/FR_CAMERA.java
+++ app/src/main/java/com/example/myapplication/FR_CAMERA.java
@@ -8,6 +8,7 @@ import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.ByteArrayInputStream;
 
 public class FR_CAMERA extends AppCompatActivity {
 
@@ -64,6 +67,7 @@ public class FR_CAMERA extends AppCompatActivity {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
         startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE);
+        //sendPictureToServer();
 
 
     }
@@ -88,6 +92,51 @@ public class FR_CAMERA extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             mImageView.setImageURI(image_uri);
+            //sendPictureToServer();
         }
     }
+
+   /** private void sendPictureToServer(){           //TODO COPY PASTED FROM NINA sendEventForm2() in AddEventActivity    //TODO Toast message if Person VALID or INVALID
+        AsyncHttpClient client = new AsyncHttpClient();
+        getAllParams();
+        RequestParams parameters = new RequestParams();
+        parameters.put("title", title);
+        parameters.put("description", description);
+        parameters.put("location", location);
+        for (Bitmap butt : listOfGGuest){
+            parameters.put("speaker_images[]", new ByteArrayInputStream(bitmapToByteArray(butt)));
+        }
+        parameters.put("picture", new ByteArrayInputStream(bitmapToByteArray(eventImage)));
+        parameters.put("start", start);
+        parameters.put("end", end);
+
+        client.post("http://infosys-mock.ap-southeast-1.elasticbeanstalk.com/api/events", parameters, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                CharSequence c = "Http successful";
+                Toast.makeText(getApplicationContext(), c, Toast.LENGTH_LONG).show();
+                // called when response HTTP status is "200 OK"
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                CharSequence c = "Http failed";
+                Toast.makeText(getApplicationContext(), c, Toast.LENGTH_LONG).show();
+                //Toast.makeText(getApplicationContext(), errorResponse.toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+
+            }
+        });
+    }  */
 }
